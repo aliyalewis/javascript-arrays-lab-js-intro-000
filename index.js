0fa51eb33abac27326e7da57f3eab11c9f08e4f3
Aliya Lewis <aliyalewis5@gmail.com>
2019-03-22 16:31:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,11 +28,6 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  kittens.slice(0, kittens.length -1)
-  return kittens
-}
-
-function removeFirstKitten() {
-  kittens.slice(1);
+  kittens.slice(0, kittens.length -1);
   return kittens
 }
